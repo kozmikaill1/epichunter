7b249b17708b884ab18a0a2f584653d66e227ccd
kozmikaill1 <ahmetlerbirliksex@gmail.com>
2025-06-23 21:00:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
     console.log(`Bot is online as ${client.user.tag}`);
-    client.user.setActivity('beta v.1.0.3', { type: ActivityType.Watching });
+    client.user.setActivity('Kedyland', { type: ActivityType.WATCHING });
 
     db.init();
 });
